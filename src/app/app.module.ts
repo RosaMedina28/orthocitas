05f28e7c83cf028c3a2b38cfaded4878774cea9b
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-06-08 16:06:57 -0500

    Eliminando y añadiendo componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+//Modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//Componentes
+import { RegistroComponent } from './Component/Registro/registro/registro.component';
+import { LoginComponent } from './Component/Login/login/login.component';
+import { NavbarComponent } from './Component/Navbar/navbar/navbar.component';
+import { InicioComponent } from './Component/Inicio/inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    //Componentes
+    RegistroComponent,
+    LoginComponent,
+    NavbarComponent,
+    InicioComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [InicioComponent] //Componente inicial 
 })
 export class AppModule { }
